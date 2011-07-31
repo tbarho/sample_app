b20f046f2c2c5fedf823ae0246d6758d45bf6881
Ty Barho <t.barho@gmail.com>
2011-07-31 04:07:59 +0000

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Ty Barho"
+  user.email                  "ty@example.com"
+  user.password               "testers"
+  user.password_confirmation  "testers"
+end
