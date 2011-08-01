6129e625d09ceeb1781dad27fb5c4265d20db0ce
Ty Barho <t.barho@gmail.com>
2011-08-01 21:02:46 +0000

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,6 @@ describe "Users" do
   describe "signup" do
 
     describe "failure" do
-
       it "should not make a new user" do
         lambda do
           visit signup_path
@@ -18,11 +17,9 @@ describe "Users" do
           response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
       end
-
     end
 
     describe "success" do
-
       it "should make a new user" do
         lambda do
           visit signup_path
@@ -35,9 +32,30 @@ describe "Users" do
           response.should have_selector("div.flash.success", :content => "Welcome")
         end.should change(User, :count).by(1)
       end
-
     end
 
   end  
+
+  describe "sign in/out" do
+
+    describe "failure" do
+      it "should not sign a user in" do
+        user = User.new(:email => "", :password => "")
+        integration_sign_in(user)
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        integration_sign_in(user)
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+
+  end
 
 end
