adae22771e83430c1ca5a72b9643fdc1978d3093
Ty Barho <t.barho@gmail.com>
2011-08-08 20:48:49 +0000

    Added user following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,51 +8,76 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+    describe "when not signed in" do
+      before(:each) do
+        get :home
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title", :content => "#{@base_title} | Home")
+      end
     end
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", :content => @base_title + " | Home")
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
     end
   end
 
   describe "GET 'contact'" do
+    before(:each) do
+      get :contact
+    end
+
     it "should be successful" do
-      get 'contact'
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'contact'
       response.should have_selector("title", :content => @base_title + " | Contact") 
     end
   end
   
   describe "GET 'about'" do
+    before(:each) do
+      get :about
+    end
+
     it "should be successful" do
-      get 'about'
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'about'
       response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
   describe "GET 'help'" do
+    before(:each) do
+      get :help
+    end
+
     it "should be successful" do
-      get 'help'
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'help'
       response.should have_selector("title", :content => @base_title + " | Help")
     end
   end
-
 end
